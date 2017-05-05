ff1852868aca9304337860b294c2df8921e0b208
evandrorisso <evandrohrisso@gmail.com>
2017-05-05 20:09:09 -0300

    NPM

--- Controller/HomeController.php
+++ Controller/HomeController.php
@@ -1,23 +1,18 @@
 <?php  
-class HomeController{
-	
-	public function Home(){
-		include("View/Home/Header.php");
-		include("View/inicio.php");
-		include("View/Home/Footer.php");
-        include("View/Home/js.php");
+	class HomeController{
+		
+		public function Home(){
+			include("View/Header.php");
+			include("View/inicio.php");
+			include("View/Footer.php");
+	        include("View/js.php");
+		}
+		public function SobreNos(){
+			include("View/Home/Header.php");
+			include("View/Home/Menu.php");
+			include("View/Home/Sobre.php");
+			include("View/Home/Footer.php");
+	        include("View/Home/js.php");
+		}
 	}
-	public function SobreNos(){
-		include("View/Home/Header.php");
-		include("View/Home/Menu.php");
-		include("View/Home/Sobre.php");
-		include("View/Home/Footer.php");
-        include("View/Home/js.php");
-	}
-}
-
-
-
-
-
 ?>
---
+++ View/Header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>EJC - Belém - IASD</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="../../Componentes/bootstrap/dist/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="../../Componentes/bootstrap/dist/css/bootstrap-theme.min.css">
+
+</head>
